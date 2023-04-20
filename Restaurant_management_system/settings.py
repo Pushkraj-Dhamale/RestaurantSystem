3418c988077163ed51b6910810a19c48743ebbb8
Pushkraj Dhamale <102896317+Pushkraj-Dhamale@users.noreply.github.com>
2023-04-20 21:48:11 +0530

    Update settings.py

--- Restaurant_management_system/settings.py
+++ Restaurant_management_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hz8(ou+a7kq72y2@$*w1uhmo)dy_g++9lul1p*+mw%=jgw5(5f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 
 ]
 
